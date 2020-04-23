0886c9dfae1391cc0d5fb68a661253d3f2254a38
maximfreerider <ivanovma41@gmail.com>
2020-04-23 13:48:01 +0300

    created CRUD operations for accounts

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,14 @@ from app import views
 
 urlpatterns = [
     path("posts/", views.PostView.as_view()),
-    path("post/create/", views.CreatePost.as_view(), name='add_post'),
+    path("post/create/", views.CreatePost.as_view()),
     path("post/<int:pk>/", views.DetailPost.as_view()),
     path("post/update/<int:pk>/", views.UpdatePost.as_view(), name='post_update_url'),
     path("post/delete/<int:pk>/", views.DeletePost.as_view(), name='post_delete_url'),
+
+    path("accounts/", views.AccountView.as_view()),
+    path("account/create/", views.CreateAcccount.as_view()),
+    path("account/<int:pk>/", views.DetailAccount.as_view()),
+    path("account/delete/<int:pk>/", views.DeleteAccount.as_view(), name='account_delete_url'),
+    path("account/update/<int:pk>/", views.UpdateAccount.as_view(), name='account_update_url'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
-from app.forms import PostForm
+from app.forms import PostForm, AccountForm
 from app.models import Post, Account
 
 
@@ -27,10 +27,27 @@ class CreatePost(View):
         return redirect("/posts/")
 
 
+class CreateAcccount(View):
+    def get(self, request):
+        return render(request, "app/account_creation.html", {})
+
+    def post(self, request):
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/accounts/")
+
+
 class DetailPost(View):
     def get(self, request, pk):
         post = Post.objects.get(id=pk)
         return render(request, "app/post_detail.html", {"post": post})
+
+
+class DetailAccount(View):
+    def get(self, request, pk):
+        account = Account.objects.get(id=pk)
+        return render(request, "app/account_detail.html", {"account": account})
 
 
 class DeletePost(View):
@@ -44,11 +61,22 @@ class DeletePost(View):
         return redirect("/posts/")
 
 
+class DeleteAccount(View):
+    def get(self, request, pk):
+        account = Account.objects.get(id=pk)
+        return render(request, "app/account_delete.html", {"account": account})
+
+    def post(self, request, pk):
+        account = Account.objects.get(id=pk)
+        account.delete()
+        return redirect("/accounts/")
+
+
 class UpdatePost(View):
     def get(self, request, pk):
         post = Post.objects.get(id=pk)
         b_form = PostForm(instance=post)
-        return render(request, 'app/update_post.html', {"form":b_form, "pk": pk})
+        return render(request, 'app/update_post.html', {"form": b_form, "pk": pk})
 
     def post(self, request, pk):
         post = Post.objects.get(id=pk)
@@ -56,3 +84,17 @@ class UpdatePost(View):
         if updated_post.is_valid():
             updated_post.save()
             return redirect(f"/posts/")
+
+
+class UpdateAccount(View):
+    def get(self, request, pk):
+        account = Account.objects.get(id=pk)
+        b_form = AccountForm(instance=account)
+        return render(request, "app/account_update.html", {"form": b_form, "pk": pk})
+
+    def post(self, request, pk):
+        account = Account.objects.get(id=pk)
+        updated_account = AccountForm(request.POST, instance=account)
+        if updated_account.is_valid():
+            updated_account.save()
+        return redirect("/accounts/")
